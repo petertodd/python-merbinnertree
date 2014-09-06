430c3e4be9e495a9e50f30b3aff16188b46ded51
Peter Todd <pete@petertodd.org>
2014-09-06 01:25:54 -0400

    Implement first part of pruning
    _mt_get(), _mt_put(), and _mt_remove() all replaced by multikey and
    proof-capable _mt_get_keys() and _mt_put_keys()
    
    Previous unit tests passing; haven't checked any of the pruning code
    itself yet.

--- merbinnertree/__init__.py
+++ merbinnertree/__init__.py
@@ -65,26 +65,60 @@ def make_MerbinnerTree_baseclass(basecls=object):
             """Calculate the data that is hashed to produce the node hash"""
             raise NotImplementedError
 
-        def _mt_get(self, keys, depth):
-            """Internal implementation of get()"""
+        def _mt_get_keys(self, result, keys, depth, prove):
+            """Internal: get keys from a branch in the tree
+
+            For each matching InnerNode result[key] is set to that node, which
+            may or may not be pruned. Missing keys do *not* raise an error.
+
+            Returns pruned_tree if prove=True, where pruned_tree is a pruned
+            version of self that can satisfy the request.
+            """
             raise NotImplementedError
+
+        def prove_existence(self, keys):
+            """Prove the existence or non-existence of one or more keys in the tree"""
 
         def __getitem__(self, key):
             """Return value associated with key"""
             self.check_key(key)
-            return self._mt_get(key, 0)
+            result = {}
+            self._mt_get_keys(result, (key,), 0, False)
+
+            # Note how if the key is not found, this will return the KeyError
+            # for us.
+            found_node = result[key]
+
+            try:
+                return found_node.value
+            except AttributeError:
+                # Node was pruned, so we don't have the value available to us
+                # even though we could confirm that the key is present in the
+                # tree.
+                assert isinstance(found_node, self.PrunedLeafNodeClass)
+                raise PrunedError
 
         def __contains__(self, key):
-            # FIXME: working?
+            self.check_key(key)
+            result = {}
+            self._mt_get_keys(result, (key,), 0, False)
+
             try:
-                self[key]
+                found_node = result[key]
             except KeyError:
+                # FIXME: do we really need to do this?
                 return False
             else:
                 return True
 
-        def _mt_put(self, leaf_node, depth):
-            """Internal implementation of put() and put_value_hash()"""
+        def _mt_put_keys(self, changed_keys, items, depth, prove):
+            """Internal: change key(s) to specified node(s)
+
+            Changing a key to an EmptyNodeClass instance has the effect of
+            removing it.
+
+            Returns (new_tree, pruned_tree)
+            """
             raise NotImplementedError
 
         def put(self, key, value):
@@ -94,8 +128,18 @@ def make_MerbinnerTree_baseclass(basecls=object):
             """
             self.check_key(key)
             self.check_value(value)
+
             leaf_node = self.FullLeafNodeClass(key, value)
-            return self._mt_put(leaf_node, 0)
+
+            changed_keys = set()
+            (new_tree, ignored) = self._mt_put_keys(changed_keys, [(key, leaf_node)], 0, False)
+            assert len(changed_keys) <= 1
+
+            # Check that the put was successful
+            if key not in changed_keys:
+                raise KeyError(key)
+
+            return new_tree
 
         def put_value_hash(self, key, value_hash):
             """Set key to a value hash
@@ -103,21 +147,20 @@ def make_MerbinnerTree_baseclass(basecls=object):
             Returns a new tree with that key set. This tree will be a pruned
             tree.
             """
-            self.check_key(key)
-            self.check_value_hash(value_hash)
-            leaf_node = self.PrunedLeafNodeClass(key, value_hash)
-            return self._mt_put(key, leaf_node, 0)
-
-
-        def _mt_remove(self, key, depth):
-            """Internal implementation of remove()"""
             raise NotImplementedError
 
         def remove(self, key):
             """Remove key from tree"""
             self.check_key(key)
-            return self._mt_remove(key, 0)
 
+            changed_keys = set()
+            (new_tree, ignored) = self._mt_put_keys(changed_keys, [(key, self.EmptyNodeClass())], 0, False)
+
+            # Check that the remove was succesful
+            if key not in changed_keys:
+                raise KeyError(key)
+
+            return new_tree
 
         def _mt_update(self, tree, depth):
             """Internal implementation of update()"""
@@ -184,14 +227,36 @@ def make_MerbinnerTree_class(treecls):
 
             return cls.__instance
 
-        def _mt_get(self, key, depth):
-            raise KeyError(key)
+        def _mt_get_keys(self, result, keys, depth, prove):
+            # Regardless of what keys are being requested, if any, return self
+            # as we can prove the existence or non-existence of that set of
+            # keys.
+            return self
 
-        def _mt_put(self, leaf_node, depth):
-            return leaf_node
+        def _mt_put_keys(self, changed_keys, items, depth, prove):
+            # There's no keys in this part of the tree, so any non-empty items
+            # can be simply passed to _mt_from_leaf_nodes() to create a new
+            # tree populated with them. Empty items meanwhile indicate that the
+            # callee was attempting to remove a key from the tree, which
+            # obviously failed.
 
-        def _mt_remove(self, key, depth):
-            raise KeyError(key)
+            leaf_nodes = []
+            # Remember that the EmptyNodeClass is a singleton.
+            for key, node in items:
+                if node is not self:
+                    changed_keys.add(key)
+                    leaf_nodes.append(node)
+
+            # _mt_from_leaf_nodes() handles the empty and len(leaf_nodes) == 1
+            # cases for us. new_tree is replacing us, so the correct depth is
+            # our depth, not depth+1
+            new_tree = self.InnerNodeClass._mt_from_leaf_nodes(leaf_nodes, depth)
+
+            # In all circumstances the pruned tree required to
+            # perform this operation is ourselves - you can always add keys to
+            # an empty node.
+            return (new_tree, self)
+
 
         def _mt_update(self, tree, depth):
             return tree
@@ -250,37 +315,67 @@ def make_MerbinnerTree_class(treecls):
             else:
                 return cls.EmptyNodeClass()
 
-        def _mt_get(self, key, depth):
-            if self.key_side(key, depth):
-                return self.left._mt_get(key, depth+1)
+        def _mt_get_keys(self, result, keys, depth, prove):
+            if len(keys):
+                left_keys = []
+                right_keys = []
+
+                for key in keys:
+                    if self.key_side(key, depth):
+                        left_keys.append(key)
+                    else:
+                        right_keys.append(key)
+
+                pruned_left_node = self.left._mt_get_keys(result, left_keys, depth+1, prove)
+                pruned_right_node = self.right._mt_get_keys(result, right_keys, depth+1, prove)
+
+                if prove:
+                    return self.InnerNodeClass(pruned_left_node, pruned_right_node)
+
+            elif prove:
+                return self.PrunedInnerNodeClass.from_inner_node(self)
+
+        def _mt_put_keys(self, changed_keys, items, depth, prove):
+            if len(items):
+                # Split items up into left and right
+                left_items = []
+                right_items = []
+                for item in items:
+                    if self.key_side(item[0], depth):
+                        left_items.append(item)
+                    else:
+                        right_items.append(item)
+
+                # Our left and right sides can now recursively handle left and
+                # right items
+                new_left_node, pruned_left_node = self.left._mt_put_keys(changed_keys, left_items, depth+1, prove)
+                new_right_node, pruned_right_node = self.right._mt_put_keys(changed_keys, right_items, depth+1, prove)
+
+                # It's possible nothing has changed if the callee was trying to
+                # remove items that aren't present in the tree. Check for that
+                # case - cheap! - and don't unnecessarily create new objects.
+                new_node = self
+                if new_left_node is not self.left or new_right_node is not self.right:
+                    new_node = self.InnerNodeClass(new_left_node, new_right_node)
+
+                pruned_node = None
+                if prove:
+                    # The pruned_node necessary to prove this put may also be
+                    # unchanged.
+                    if pruned_left_node is not self.left or pruned_right_node is not self.right:
+                        pruned_node = self.InnerNodeClass(pruned_left_node, pruned_right_node)
+
+                return (new_node, pruned_node)
+
             else:
-                return self.right._mt_get(key, depth+1)
+                pruned_node = None
+                if prove:
+                    # No items were changed, which means the minimum
+                    # information to prove that is the pruned version of
+                    # ourselves.
+                    pruned_node = self.PrunedInnerNodeClass.from_inner_node(self)
 
-        def _mt_put(self, leaf_node, depth):
-            new_left = self.left
-            new_right = self.right
-
-            if self.key_side(leaf_node.key, depth):
-                new_left = self.left._mt_put(leaf_node, depth+1)
-            else:
-                new_right = self.right._mt_put(leaf_node, depth+1)
-
-            if (new_left is not self.left) or (new_right is not self.right):
-                return self.InnerNodeClass(new_left, new_right)
-            else:
-                return self
-
-        def _mt_remove(self, key, depth):
-            new_left = self.left
-            new_right = self.right
-
-            if self.key_side(key, depth):
-                new_left = self.left._mt_remove(key, depth+1)
-            else:
-                new_right = self.right._mt_remove(key, depth+1)
-
-            assert new_left is not self.left or new_right is not self.right
-            return self.InnerNodeClass(new_left, new_right)
+                return (self, pruned_node)
 
         def _mt_update(self, tree, depth):
             raise NotImplementedError
@@ -304,14 +399,24 @@ def make_MerbinnerTree_class(treecls):
             object.__setattr__(self, 'pruned_hash', pruned_hash)
             return self
 
-        def _mt_get(self, key, depth):
+        def _mt_get_keys(self, key, depth):
             raise self.PrunedError('get', key, depth)
 
-        def _mt_put(self, leaf_node, depth):
-            raise self.PrunedError('set', key, depth)
+        def _mt_get_keys(self, result, keys, depth, ignore_missing=False, prove=False):
+            if len(keys):
+                raise self.PrunedError('get', key, depth)
+            else:
+                return self
 
-        def _mt_remove(self, key, depth):
-            raise self.PrunedError('remove', key, depth)
+        def _mt_put_keys(self, changed_keys, items, depth, prove):
+            if len(items):
+                # We're pruned, so we don't have the information necessary to
+                # change anything in this part of the tree.
+                raise self.PrunedError('set', key, depth)
+
+            else:
+                # However we do have the information necessary to do nothing.
+                return (self, self)
 
         def _mt_update(self, tree, depth):
             if self.hash == tree.hash:
@@ -338,21 +443,56 @@ def make_MerbinnerTree_class(treecls):
 
     class MerbinnerTreeLeafNodeClass(treecls):
         __slots__ = ['key']
-        def _mt_put(self, leaf_node, depth):
-            # FIXME: return self if both are exactly equal
-            if self.key == leaf_node.key:
-                return leaf_node
 
-            else:
-                # Note how the depth is *not* increased by one, as the inner
-                # node is replacing the node at this level.
-                return self.InnerNodeClass._mt_from_leaf_nodes((self, leaf_node), depth)
+        def _mt_get_keys_common(self, result, keys, depth, ignore_missing=False, prove=False):
+            found_match = False
+            for key in keys:
+                if self.key == key:
+                    found_match = True
+                    result[key] = self
+                    break
 
-        def _mt_remove(self, key, depth):
-            if self.key == key:
-                return self.EmptyNodeClass()
-            else:
-                raise KeyError(key)
+            return found_match
+
+        def _mt_put_keys(self, changed_keys, items, depth, prove):
+            # Similar to the EmptyNode implementation, we can let
+            # _mt_from_leaf_nodes() do all the real work.
+
+            # All empty items where the callee was trying to remove a key from
+            # the tree can be filtered out of items as those keys obviously
+            # don't exist.
+            add_ourself = True
+            leaf_nodes = []
+            for key, new_node in items:
+                # If our key is present in items we're being modified, so we
+                # don't want to add ourselves to the list of leaf_nodes
+                if add_ourself and self.key == key:
+                    # This does however mean that our key is now changed.
+                    changed_keys.add(key)
+                    add_ourself = False
+
+                if not isinstance(new_node, self.EmptyNodeClass):
+                    changed_keys.add(key)
+                    leaf_nodes.append(new_node)
+
+            if add_ourself:
+                leaf_nodes = [self] + leaf_nodes
+
+            # _mt_from_leaf_nodes() handles the empty and len(leaf_nodes) == 1
+            # cases for us. new_tree is replacing us, so the correct depth is
+            # our depth, not depth+1
+            new_tree = self.InnerNodeClass._mt_from_leaf_nodes(leaf_nodes, depth)
+
+            pruned_tree = None
+            if prove:
+                # The pruned version of this node is the information necessary
+                # to perform this put operation.
+                if isinstance(pruned_tree, self.FullLeafNodeClass):
+                    pruned_tree = self.PrunedLeafNodeClass.from_FullLeafNode(self)
+                else:
+                    pruned_tree = self
+
+            return (new_tree, pruned_tree)
 
         def _mt_update(self, tree, depth):
             if self.hash == tree.hash:
@@ -376,17 +516,13 @@ def make_MerbinnerTree_class(treecls):
             object.__setattr__(self, 'value', value)
             return self
 
-        def _mt_get(self, key, depth):
-            if self.key == key:
-                return self.value
-            else:
-                raise KeyError(key)
+        def _mt_get_keys(self, result, keys, depth, ignore_missing=False, prove=False):
+            found_match = self._mt_get_keys_common(result, keys, depth, ignore_missing, prove)
+            if found_match:
+                return self
 
-        def _mt_remove(self, key, depth):
-            if self.key == key:
-                return self.EmptyNodeClass()
-            else:
-                raise KeyError(key)
+            elif prove:
+                return self.PrunedLeafNodeClass.from_FullLeafNode(self)
 
         def _mt_merge(self, tree, depth):
             # merge() checked that self and tree are the same, so tree must be
@@ -404,23 +540,22 @@ def make_MerbinnerTree_class(treecls):
 
     class MerbinnerTreePrunedLeafNodeClass(MerbinnerTreeLeafNodeClass):
         __slots__ = ['value_hash']
-        def __new__(cls, key, value):
+        def __new__(cls, key, value_hash):
             self = object.__new__(cls)
             object.__setattr__(self, 'key', key)
-            object.__setattr__(self, 'value', value)
+            object.__setattr__(self, 'value_hash', value_hash)
             return self
 
-        def _mt_get(self, key, depth):
-            if self.key == key:
-                return self.value
-            else:
-                raise KeyError(key)
+        @classmethod
+        def from_FullLeafNode(cls, full_leaf_node):
+            return cls(full_leaf_node.key, cls.calc_value_hash(full_leaf_node.value))
 
-        def _mt_remove(self, key, depth):
-            if self.key == key:
-                return self.EmptyNodeClass()
-            else:
-                raise KeyError(key)
+        def _mt_get_keys(self, result, keys, depth, ignore_missing=False, prove=False):
+            self._mt_get_keys_common(result, keys, depth, ignore_missing, prove)
+
+            # Whether or not a match is found is irrelevant; the best we can do
+            # is return ourselves as we're pruned.
+            return self
 
         def _mt_merge(self, tree, depth):
             # merge() checked that self and tree are the same, so tree must be
